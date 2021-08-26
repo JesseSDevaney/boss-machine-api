1f3ebc7294df799683f434ea10c966b63b6fa19d
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2021-08-26 14:45:16 -0400

    Create GET /api/minions/:minionId endpoint
    GET requests to /api/minions/:minionId will result in:
    - a 200 response with the minion that has that id
    - else, a 404 error if the minion with the given id does not exist

--- server/routes/minions.cjs
+++ server/routes/minions.cjs
@@ -1,5 +1,10 @@
 const express = require("express");
+const {
+  addToDatabase,
+  getAllFromDatabase,
+  getFromDatabaseById,
 const { addToDatabase, getAllFromDatabase } = require("../db.cjs");
+} = require("../db.cjs");
 const minionsRouter = express.Router();
 
 minionsRouter.get("/", (req, res) => {
@@ -15,6 +20,22 @@ minionsRouter.post("/", (req, res, next) => {
     err.status = 400;
     next(err);
   }
+});
+
+minionsRouter.param("minionId", (req, res, next, id) => {
+  const minion = getFromDatabaseById("minions", id);
+  if (!minion) {
+    const err = new Error(`Minion #${id} does not exist`);
+    err.status = 404;
+    return next(err);
+  }
+
+  req.minion = minion;
+  next();
+});
+
+minionsRouter.get("/:minionId", (req, res) => {
+  res.status(200).json(req.minion);
 });
 
 module.exports = minionsRouter;
