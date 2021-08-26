2b17516333383edf4a9074e835e1d836d98eed91
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2021-08-26 17:24:51 -0400

    Create POST /api/meetings route
    - POST requests to /api/meetings will automatically create, add to the database, and respond with
    a new meeting using createMeeting() in db.cjs

--- server/routes/meetings.cjs
+++ server/routes/meetings.cjs
@@ -1,10 +1,26 @@
 const express = require("express");
 const meetingsRouter = express.Router();
-const { getAllFromDatabase } = require("../db.cjs");
+const {
+  addToDatabase,
+  createMeeting,
+  getAllFromDatabase,
+} = require("../db.cjs");
+
+const isValidDate = (date) => !Number.isNaN(Date.parse(date));
 
 meetingsRouter.get("/", (req, res) => {
   const meetings = getAllFromDatabase("meetings");
   res.status(200).json(meetings);
+});
+
+meetingsRouter.post("/", (req, res, next) => {
+  try {
+    const newMeeting = addToDatabase("meetings", createMeeting());
+    res.status(201).json(newMeeting);
+  } catch (err) {
+    err.status = 400;
+    next(err);
+  }
 });
 
 module.exports = meetingsRouter;
