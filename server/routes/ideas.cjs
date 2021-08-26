58dff6fcedc016e1cd41793325e40689e4807c35
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2021-08-26 15:07:07 -0400

    Create POST /api/ideas route
    - When given the proper shape for idea, POST requests to /api/ideas will add an idea to the ideas database,
    and send a JSON response with that new idea.
    - Else, an error will be thrown with status 400 and handled by error handling middleware.

--- server/routes/ideas.cjs
+++ server/routes/ideas.cjs
@@ -1,10 +1,20 @@
 const express = require("express");
 const ideasRouter = express.Router();
-const { getAllFromDatabase } = require("../db.cjs");
+const { addToDatabase, getAllFromDatabase } = require("../db.cjs");
 
 ideasRouter.get("/", (req, res) => {
   const ideas = getAllFromDatabase("ideas");
   res.status(200).json(ideas);
+});
+
+ideasRouter.post("/", (req, res, next) => {
+  try {
+    const newIdea = addToDatabase("ideas", req.body);
+    res.status(201).json(newIdea);
+  } catch (err) {
+    err.status = 400;
+    next(err);
+  }
 });
 
 module.exports = ideasRouter;
