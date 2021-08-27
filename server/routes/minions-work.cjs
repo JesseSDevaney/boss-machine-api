5cf00640085e3f696ad8766af8f9ba1dbd0ef6e3
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2021-08-26 20:08:32 -0400

    Create POST /api/minions/:minionId/work route
    - When given the proper shape for work, POST requests to /api/minions/:minionId/work will add a work item to the
    work database, and send a JSON response with that new work item.
    - Else, an error will be thrown with status 400 and handled by error handling middleware.

--- server/routes/minions-work.cjs
+++ server/routes/minions-work.cjs
@@ -1,5 +1,9 @@
 const express = require("express");
-const { getAllFromDatabase, getFromDatabaseById } = require("../db.cjs");
+const {
+  addToDatabase,
+  getAllFromDatabase,
+  getFromDatabaseById,
+} = require("../db.cjs");
 const workRouter = express.Router({ mergeParams: true });
 
 workRouter.get("/", (req, res) => {
@@ -8,6 +12,17 @@ workRouter.get("/", (req, res) => {
     ({ minionId }) => minionId === req.minion.id
   );
   res.status(200).json(minionWork);
+});
+
+workRouter.post("/", (req, res, next) => {
+  try {
+    req.body.minionId = req.minion.id;
+    const newWork = addToDatabase("work", req.body);
+    res.status(201).json(newWork);
+  } catch (err) {
+    err.status = 400;
+    next(err);
+  }
 });
 
 module.exports = workRouter;
